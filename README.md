95f75739f3d29c731362d2d2b7c0d024653891a9
Iain Maryanow <iainmaryanow@gmail.com>
2022-06-19 10:44:12 +0200

    Add timeout expiration label

--- README.md
+++ README.md
@@ -5,4 +5,3 @@ TODO:
 Error handling in the UI
 Frontend checks for required values
 Only allow increments to the specified key instead of being able to change the key input
-Add timeout UI
--- src/cache/key-values/Countdown.jsx
+++ src/cache/key-values/Countdown.jsx
@@ -1,17 +1,22 @@
 import { useEffect, useState } from 'react'
+import { Label } from 'semantic-ui-react'
 
 const Countdown = ({ startingValue }) => {
   const [currentValue, setCurrentValue] = useState(startingValue)
 
   useEffect(() => {
-    const id = setInterval(() => {
-      setCurrentValue(currentValue => currentValue - 1)
-    }, 1000)
-
+    const id = setInterval(() => setCurrentValue(currentValue => currentValue - 1), 1000)
     return () => clearInterval(id)
   }, [])
 
-  return <span>Expires in {currentValue}s</span>
+  let text = `Expires in ${currentValue}s`
+  let color = 'yellow'
+  if (currentValue <= 0) {
+    text = 'Expired'
+    color = 'red'
+  }
+
+  return <Label size='tiny' color={color}>{text}</Label>
 }
 
 export default Countdown
